52a84deec5c2e311ad9249acce6acc885f4100df
Dongzi Qu <61256557+Dongzi-dq394@users.noreply.github.com>
2020-05-10 22:05:38 -0400

    Add more annotations to predict.py

--- Code/predict.py
+++ Code/predict.py
@@ -1,7 +1,7 @@
-#--------------------------------------------------------#
-## Use created database to predict the new gestures     ##
-## The code creating model will be uploaded by notebook ##
-#--------------------------------------------------------#
+#-------------------------------------------------------------------------------#
+## Use this code to predict the incoming gestures via a trained model          ##
+## The model is constructed and trained mainly based on VGG16 with more layers ##
+#-------------------------------------------------------------------------------#
 
 import numpy as np  ## vesion: 1.18.3
 import cv2  ## version: 4.1.2
@@ -10,26 +10,34 @@ import imutils  ## version: 0.5.3
 import os
 import sys
 
+# keras version: 2.3.1
 from keras.models import load_model
 from keras.preprocessing.image import image
 from keras.preprocessing.image import img_to_array
 from keras.applications.vgg16 import preprocess_input
 
+# Use a var outside the excution in order to keep the name between two predictions
 gesture_name = None
 
+# Use this function to predict
 def predict_gesture(input_image, model):
-	x = input_image.copy()
-	x = cv2.resize(x, (224,224))
+	x = input_image.copy() # Not change the original image
+
+	# Resize the image to fit our pre-trained model
+	# This part are relied on the model itself
+	x = cv2.resize(x, (224,224)) 
 	x = np.stack((x,)*3, axis=-1)
 	x = np.expand_dims(x, axis=0)
 	x = preprocess_input(x)
 
 	result = model.predict(x)
-	result = result.flatten()
-	return np.argmax(result)
+	result = result.flatten() # shape from (5,1) to (5,)
+	return np.argmax(result) # output the index with the largest value
 
+# load the trained model
 model = load_model('models/saved_model.hdf5')
 
+# mapping between the index and gesture name
 gesture_map = ['Fist', 'Rock', 'OK', 'Palm', 'Victory']
 
 ## global variable for the background
@@ -45,7 +53,7 @@ def get_bg(image, W):
 	cv2.accumulateWeighted(image, background, W)
 
 ## use this function to get the thresholded image and segmented image for hand
-def seg_threshold(image, threshold=50):
+def seg_threshold(image, threshold=5):
 	global background
 
 	# substraction between background and the hand image
@@ -120,31 +128,19 @@ if __name__ == "__main__":
 			cv2.rectangle(clone, (box_left, box_top), (box_right, box_bottom), (0,255,0), 3)
 			cv2.putText(clone, 'Please put hand in green box', (20,30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
 
-			# the details for the max number and the number for each gesture
-			#cv2.putText(clone, 'Max number for each image: %d' % (MAX_NUM), (20,65), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-			# you can see the information on screen
-			# when you want to save images, please keep pressing the key
-			# cv2.putText(clone, 'Press v to save Victory; Cur Number: %d' % (num_picture['v']), (20,100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-			# cv2.putText(clone, 'Press o to save OK; Cur Number: %d' % (num_picture['o']), (20,135), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-			# cv2.putText(clone, 'Press h to save Horn; Cur Number: %d' % (num_picture['h']), (20,170), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-			# cv2.putText(clone, 'Press f to save Fist; Cur Number: %d' % (num_picture['f']), (20,205), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-			# cv2.putText(clone, 'Press p to save Palm; Cur Number: %d' % (num_picture['p']), (20,240), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-
+			# Extract the hand region from the ROI
 			gesture_seg = seg_threshold(gray_ROI)
 
-			# only if there IS a hand!
+			# only if there is a hand!!!
 			if gesture_seg:
 				(thresh, seg) = gesture_seg
 
-				## New
-				# To avoid the latency
-				if (num_frame - 50) % 15 == 0:
+				## New added
+				# We predict the gesture each 10 frames to avoid the latency
+				if (num_frame - 50) % 10 == 0:
 					index = predict_gesture(thresh, model)
 					gesture_name = gesture_map[index]
 
-					#cv2.putText(clone, 'This gesture is: %s' % (gesture_name), (20,65), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-				#index = predict_gesture(thresh, model)
-				#gesture_name = gesture_map[index]
 				cv2.putText(clone, 'This gesture is: %s' % (gesture_name), (20,65), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
 
 				# draw a contour for the hand in clone frame
@@ -152,8 +148,6 @@ if __name__ == "__main__":
 				# show the thresholded iamge for hand in a separated window
 				cv2.imshow("Thresholding", thresh)
 
-				# same trick, avoid modifying
-				# saved_image = thresh.copy()
 		
 		# show the clone frame in a separated window
 		cv2.imshow('Frame', clone)
