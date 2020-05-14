8320f1e8ca0eeda0004aaf2e698777928138cb9d
Dongzi Qu <61256557+Dongzi-dq394@users.noreply.github.com>
2020-05-13 20:28:37 -0400

    recalibration added

--- Code/build_db.py
+++ Code/build_db.py
@@ -9,8 +9,11 @@ import imutils  ## version: 0.5.3
 import os
 import sys
 
-## global variable for the background
-background = None
+background = None # global variable for the background
+RESET = False # global vaiable for recompute the background
+cur_frame = None # the frame when press 'r' to reset
+
+## Use this function to compute the initial background
 def get_bg(image, W):
 	global background
 	# for the very beginning case
@@ -22,7 +25,7 @@ def get_bg(image, W):
 	cv2.accumulateWeighted(image, background, W)
 
 ## use this function to get the thresholded image and segmented image for hand
-def seg_threshold(image, threshold=30):
+def seg_threshold(image, threshold=50):
 	global background
 
 	# substraction between background and the hand image
@@ -62,7 +65,7 @@ if __name__ == "__main__":
 	# more kinds of gestures can be added
 	num_picture = {'v':0, 'o':0, 'h':0, 'f':0, 'p':0}
 	# there should be a folder called 'data' before running this program
-	relative_path = './data'
+	relative_path = '../data'
 	for directory, subdir, files in os.walk(relative_path):
 		for file in files:
 			# look through all the files already inside this folder
@@ -115,33 +118,43 @@ if __name__ == "__main__":
 			get_bg(gray_ROI, 0.5)
 			cv2.putText(clone, 'Please wait for extracting bg...', (20,30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
 		else:
-			# draw a green box onto clone frame where the hand in
-			cv2.rectangle(clone, (box_left, box_top), (box_right, box_bottom), (0,255,0), 3)
-			cv2.putText(clone, 'Please put hand in green box', (20,30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+			if RESET == False:
+				# draw a green box onto clone frame where the hand in
+				cv2.rectangle(clone, (box_left, box_top), (box_right, box_bottom), (0,255,0), 3)
+				cv2.putText(clone, 'Please put hand in green box', (20,30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
 
-			# the details for the max number and the number for each gesture
-			cv2.putText(clone, 'Max number for each image: %d' % (MAX_NUM), (20,65), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-			# you can see the information on screen
-			# when you want to save images, please keep pressing the key
-			cv2.putText(clone, 'Press v to save Victory; Cur Number: %d' % (num_picture['v']), (20,100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-			cv2.putText(clone, 'Press o to save OK; Cur Number: %d' % (num_picture['o']), (20,135), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-			cv2.putText(clone, 'Press h to save Horn; Cur Number: %d' % (num_picture['h']), (20,170), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-			cv2.putText(clone, 'Press f to save Fist; Cur Number: %d' % (num_picture['f']), (20,205), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-			cv2.putText(clone, 'Press p to save Palm; Cur Number: %d' % (num_picture['p']), (20,240), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+				# the details for the max number and the number for each gesture
+				cv2.putText(clone, 'Max number for each image: %d' % (MAX_NUM), (20,65), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+				# you can see the information on screen
+				# when you want to save images, please keep pressing the key
+				cv2.putText(clone, 'Press v to save Victory; Cur Number: %d' % (num_picture['v']), (20,100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+				cv2.putText(clone, 'Press o to save OK; Cur Number: %d' % (num_picture['o']), (20,135), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+				cv2.putText(clone, 'Press h to save Horn; Cur Number: %d' % (num_picture['h']), (20,170), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+				cv2.putText(clone, 'Press f to save Fist; Cur Number: %d' % (num_picture['f']), (20,205), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+				cv2.putText(clone, 'Press p to save Palm; Cur Number: %d' % (num_picture['p']), (20,240), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+				cv2.putText(clone, 'Press r to recalibrate the background.', (20,275), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
 
-			gesture_seg = seg_threshold(gray_ROI)
+				gesture_seg = seg_threshold(gray_ROI)
 
-			# only if there IS a hand!
-			if gesture_seg:
-				(thresh, seg) = gesture_seg
+				# only if there IS a hand!
+				if gesture_seg:
+					(thresh, seg) = gesture_seg
 
-				# draw a contour for the hand in clone frame
-				cv2.drawContours(clone, [seg + (box_left, box_top)], -1, (0, 0, 255))
-				# show the thresholded iamge for hand in a separated window
-				cv2.imshow("Thresholding", thresh)
+					# draw a contour for the hand in clone frame
+					cv2.drawContours(clone, [seg + (box_left, box_top)], -1, (0, 0, 255))
+					# show the thresholded iamge for hand in a separated window
+					cv2.imshow("Thresholding", thresh)
 
-				# same trick, avoid modifying
-				saved_image = thresh.copy()
+					# same trick, avoid modifying
+					saved_image = thresh.copy()
+			else:
+				# Use 30 frames after 'r' pressed to recalibrate the background
+				if num_frame - cur_frame < 30:
+					get_bg(gray_ROI, 0.5)
+					cv2.putText(clone, 'Please wait for recalibrating bg...', (20,30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+				else:
+					# change the RESET to False
+					RESET = False
 		
 		# show the clone frame in a separated window
 		cv2.imshow('Frame', clone)
@@ -151,39 +164,44 @@ if __name__ == "__main__":
 
 		if key_input == ord('q'):
 			CONT = False
+		# reset the background
+		elif key_input == ord('r'):
+			background = None
+			cur_frame = num_frame
+			RESET = True
 		# save 'v'
 		elif key_input == ord('v'):
 			if num_picture['v'] < MAX_NUM:
 				index = num_picture['v'] + 1
-				path = "data/" + (chr(key_input)+"_"+str(index)+".jpg")
+				path = "../data/" + chr(key_input) + "/" + (chr(key_input)+"_"+str(index)+".jpg")
 				cv2.imwrite(path, saved_image)
 				num_picture['v'] += 1
 		#save 'o'
 		elif key_input == ord('o'):
 			if num_picture['o'] < MAX_NUM:
 				index = num_picture['o'] + 1
-				path = "data/" + (chr(key_input)+"_"+str(index)+".jpg")
+				path = "../data/" + chr(key_input) + "/" + (chr(key_input)+"_"+str(index)+".jpg")
 				cv2.imwrite(path, saved_image)
 				num_picture['o'] += 1
 		# save 'h'
 		elif key_input == ord('h'):
 			if num_picture['h'] < MAX_NUM:
 				index = num_picture['h'] + 1
-				path = "data/" + (chr(key_input)+"_"+str(index)+".jpg")
+				path = "../data/" + chr(key_input) + "/" + (chr(key_input)+"_"+str(index)+".jpg")
 				cv2.imwrite(path, saved_image)
 				num_picture['h'] += 1
 		# save 'f'
 		elif key_input == ord('f'):
 			if num_picture['f'] < MAX_NUM:
 				index = num_picture['f'] + 1
-				path = "data/" + (chr(key_input)+"_"+str(index)+".jpg")
+				path = "../data/" + chr(key_input) + "/" + (chr(key_input)+"_"+str(index)+".jpg")
 				cv2.imwrite(path, saved_image)
 				num_picture['f'] += 1
 		# save 'p'
 		elif key_input == ord('p'):
 			if num_picture['p'] < MAX_NUM:
 				index = num_picture['p'] + 1
-				path = "data/" + (chr(key_input)+"_"+str(index)+".jpg")
+				path = "../data/" + chr(key_input) + "/" + (chr(key_input)+"_"+str(index)+".jpg")
 				cv2.imwrite(path, saved_image)
 				num_picture['p'] += 1
 	
